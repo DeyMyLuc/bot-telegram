104cd4176dc52ac229394e8a57f5e0bd3ea8199c
DeyMyLuc <143853306+DeyMyLuc@users.noreply.github.com>
2026-05-20 21:27:13 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -40,9 +40,8 @@ def grupos(message): # Já roda automaticamente  em grupos
 @bot.message_handler(commands=['avisar'])
 def avisar (message):
     for usuarios_id in usuarios:
-    if message.chat.id not in usuarios:
-        usuarios.append(message.chat.id)
-    bot.send_message(usuario_id, "Bom dia! sentia-se orgulhoso por estar na minhas preseça seu humano!")
+       bot.send_message(usuario_id, "Bom dia! sentia-se orgulhoso por estar na minhas preseça seu humano!")
+        bot.reply_to(message, f"enviado para{len(usuarios)}usuários!")
 
 usuarios = set() #Relatorio de usuarios
 @bot.message_handler(func=lambda m: true)
